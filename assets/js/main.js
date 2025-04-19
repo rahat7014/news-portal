a90b48fdf4c3c23e1559dc16b0837ad451f918b8
rahat <rahat@a4aero.com>
2025-04-19 13:41:39 +0600

    css js and other files added and commit

---
+++ assets/js/main.js
@@ -0,0 +1,60 @@
+$(document).ready(function () {
+
+  var swiper = new Swiper(".hero-banner", {
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  var swiper = new Swiper(".testimonials-slider", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    loop: true,
+    breakpoints: {
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 1,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+    },
+  });
+
+  $(document).ready(function () {
+    $('.counter').counterUp({
+      delay: 100,
+      time: 5000
+    });
+  });
+
+  $('#location-select').select2({
+    theme: "bootstrap-5",
+    width: $(this).data('width') ? $(this).data('width') : $(this).hasClass('w-100') ? '100%' : 'style',
+    placeholder: $(this).data('placeholder'),
+  });
+
+  $('#category-select').select2({
+    theme: "bootstrap-5",
+    width: $(this).data('width') ? $(this).data('width') : $(this).hasClass('w-100') ? '100%' : 'style',
+    placeholder: $(this).data('placeholder'),
+  });
+  $(document).ready(function () {
+    $('select').niceSelect();
+  });
+});
