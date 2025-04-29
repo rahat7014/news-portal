e6ce7b166c729df26975435a081de34f15eda9c8
Rahat <rahat@a4aero.com>
2025-04-29 20:54:37 +0600

    fixed and commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -3861,5 +3861,37 @@ $(document).ready(function () {
       alert('Loading Group: ' + groupName);
     });
   });
+  ///
+  var $affectedElements = $("#description");
+
+  // Storing the original size in a data attribute so size can be reset
+  $affectedElements.each(function () {
+    var $this = $(this);
+    $this.data("orig-size", $this.css("font-size"));
+  });
+
+  $("#btn-increase").click(function () {
+    changeFontSize(1);
+  })
+
+  $("#btn-decrease").click(function () {
+    changeFontSize(-1);
+  })
+
+  $("#btn-orig").click(function () {
+    $affectedElements
+      .each(function () {
+        var $this = $(this);
+        $this.css("font-size", $this.data("orig-size"));
+      });
+  })
+
+  function changeFontSize(direction) {
+    $affectedElements
+      .each(function () {
+        var $this = $(this);
+        $this.css("font-size", parseInt($this.css("font-size")) + direction);
+      });
+  }
 
 });
